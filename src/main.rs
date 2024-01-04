85b2c6ee3356a8bea8408476b7288876959847e1
5-23 <yhanbyeol6@gmail.com>
2024-01-04 10:35:44 +0900

    feat: add boom effect

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ fn setup(mut commands: Commands, _asset_server: Res<AssetServer>) {
     commands
         .spawn(SpriteBundle {
             sprite: Sprite {
-                custom_size: Some(Vec2::new(200., 200.)),
+                custom_size: Some(Vec2::new(100., 100.)),
                 color: Color::Rgba {
                     red: 0.5,
                     green: 0.5,
@@ -65,40 +65,48 @@ fn stone_setup(
     let cursor = cursor.single();
     for (transform, mut stone) in q.iter_mut() {
         if input.just_pressed(MouseButton::Left) {
-            stone.custom_size = Some(Vec2::new(400., 400.));
-            commands
-                .spawn(SpriteBundle {
-                    sprite: Sprite {
-                        custom_size: Some(Vec2::new(10., 10.)),
-                        color: Color::Rgba {
-                            red: 0.0,
-                            green: 0.0,
-                            blue: 0.5,
-                            alpha: 0.,
+            stone.custom_size = Some(Vec2::new(50., 50.));
+
+            for __ in 0..10 {
+                let mut rng = rand::thread_rng();
+                let d = rng.gen_range(0..360) as f32;
+                commands
+                    .spawn(SpriteBundle {
+                        sprite: Sprite {
+                            custom_size: Some(Vec2::new(10., 10.)),
+                            color: Color::Rgba {
+                                red: 0.0,
+                                green: 0.0,
+                                blue: 0.5,
+                                alpha: 1.,
+                            },
+                            ..Default::default()
+                        },
+                        transform: Transform {
+                            translation: Vec3::new(0., 0., 0.),
+                            ..Default::default()
                         },
                         ..Default::default()
-                    },
-                    transform: Transform {
-                        translation: Vec3::new(0., 0., 0.),
-                        ..Default::default()
-                    },
-                    ..Default::default()
-                })
-                .insert(Ore {
-                    x: {
-                        let mut rng = rand::thread_rng();
-                        transform.translation.x + f32::sin(rng.gen_range(0..360) as f32) * 50.
-                    },
-                    y: {
-                        let mut rng = rand::thread_rng();
-                        transform.translation.y + f32::cos(rng.gen_range(0..360) as f32) * 50.
-                    },
-                });
+                    })
+                    .insert(Ore {
+                        x: {
+                            transform.translation.x + f32::sin(d) * rng.gen_range(60..120) as f32
+                        },
+                        y: {
+                            transform.translation.y + f32::cos(d) * rng.gen_range(60..120) as f32
+                        },
+                    });
+            }
         }
         let size = stone.custom_size.unwrap();
-        stone.custom_size = Some(size + ((Vec2::new(200., 200.) - size) / 30.));
+        stone.custom_size = Some(size + ((Vec2::new(100., 100.) - size) / 10.));
         // stone.custom_size = Some(size - 0.1);
     }
 }
 
-fn ore_movment() {}
+fn ore_movment(mut q: Query<(&mut Transform, &Ore), With<Ore>>) {
+    for (mut transform, ore) in q.iter_mut() {
+        let pos: Vec3 = transform.translation;
+        transform.translation += (Vec3::new(ore.x, ore.y, 0.) - pos) / 5.;
+    }
+}
